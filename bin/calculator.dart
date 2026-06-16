f3a42f4645680fc569a0efe6690a42825ece4dd1
Emmeor <emmanuelexclamadormendoza@gmail.com>
2026-06-17 02:52:00 +0800

    Implement basic calculator operations
    Added functionality to perform basic arithmetic operations and display results.

--- bin/calculator.dart
+++ bin/calculator.dart
@@ -12,6 +12,26 @@
 // Concepts to research (see the Dart cheat sheet): reading input from the
 // user, converting text into a number, arithmetic operators, and printing.
 
+import 'dart:io';
+
 void main() {
+  
   // TODO: write your solution here
+  stdout.write("Enter the first number: ");
+  double firstNumber = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter the second number: ");
+  double secondNumber = double.parse(stdin.readLineSync()!);
+
+  double sum = firstNumber + secondNumber;
+  double difference = firstNumber - secondNumber;
+  double product = firstNumber * secondNumber;
+  double quotient = firstNumber / secondNumber;
+  double average = (firstNumber + secondNumber) / 2;
+
+  print("Sum: $sum");
+  print("Difference: $difference");
+  print("Product: $product");
+  print("Quotient: $quotient");
+  print("Average: $average");
 }
